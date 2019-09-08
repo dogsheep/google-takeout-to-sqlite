231497345d5a653d5ce9b74a8c003cfe1b87530d
Simon Willison <swillison@gmail.com>
2019-09-07 20:12:50 -0700

    Order of files in zip does not matter

--- tests/test_create_zip.py
+++ tests/test_create_zip.py
@@ -4,9 +4,9 @@ from .utils import create_zip
 
 def test_create_zip():
     zf = create_zip()
-    assert [
+    assert {
         "Takeout/My Activity/Maps/My Activity.json",
         "Takeout/My Activity/Search/My Activity.json",
         "Takeout/My Activity/Google Play Books/My Activity.json",
         "Takeout/Location History/Location History.json",
-    ] == [f.filename for f in zf.filelist]
+    } == {f.filename for f in zf.filelist}

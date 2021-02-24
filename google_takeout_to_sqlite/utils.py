a3de045eba0fae4b309da21aa3119102b0efc576
David Boucha <boucha@gmail.com>
2021-02-23 17:34:16 -0700

    Format with Black

--- google_takeout_to_sqlite/utils.py
+++ google_takeout_to_sqlite/utils.py
@@ -62,9 +62,9 @@ def id_for_location_history(row):
 
 def get_mbox(mbox_file):
     num_errors = 0
-    print('Preparing to process emails...')
+    print("Preparing to process emails...")
     mbox = mailbox.mbox(mbox_file)
-    print('Processing {} emails'.format(len(mbox)))
+    print("Processing {} emails".format(len(mbox)))
 
     # These are all the Gmail email fields available
     # ['X-GM-THRID', 'X-Gmail-Labels', 'Delivered-To', 'Received', 'Received',
@@ -78,31 +78,29 @@ def get_mbox(mbox_file):
     for email in track(mbox):
         try:
             message = {}
-            message['Message-Id'] = email['Message-Id']
-            message['X-GM-THRID'] = email['X-GM-THRID']
-            message['X-Gmail-Labels'] = email['X-Gmail-Labels']
+            message["Message-Id"] = email["Message-Id"]
+            message["X-GM-THRID"] = email["X-GM-THRID"]
+            message["X-Gmail-Labels"] = email["X-Gmail-Labels"]
 
             # These following try/excepts are here because for some reason
             # these items returned from the mbox module are sometimes strings
             # and sometimes headers and sometimes None.
 
             try:
-               email['From'].decode('utf-8')
+                email["From"].decode("utf-8")
             except AttributeError:
-                message['From'] =  str(email['From'])
+                message["From"] = str(email["From"])
             try:
-                email['To'].decode('utf-8')
+                email["To"].decode("utf-8")
             except AttributeError:
-                message["To"] =  str(email['To'])
+                message["To"] = str(email["To"])
 
             try:
-                email['Subject'].decode('utf-8')
+                email["Subject"].decode("utf-8")
             except AttributeError:
-                message["Subject"] = str(email['Subject'])
+                message["Subject"] = str(email["Subject"])
 
-
-            message["date"] = get_message_date(email.get('Date'),
-                                email.get_from())
+            message["date"] = get_message_date(email.get("Date"), email.get_from())
             message["body"] = get_email_body(email)
 
             yield message
@@ -110,7 +108,7 @@ def get_mbox(mbox_file):
             # How does this project want to handle logging? For now we're just
             # printing out variables
             num_errors = num_errors + 1
-            print('Errors: {}'.format(num_errors))
+            print("Errors: {}".format(num_errors))
             print(traceback.format_exc())
             continue
 
@@ -122,41 +120,40 @@ def save_emails(db, mbox_file):
     db["mbox_emails"].upsert_all(
         (
             {
-                "id": message['Message-Id'],
-                'X-GM-THRID': message['X-GM-THRID'],
-                'X-Gmail-Labels': message['X-Gmail-Labels'],
-                "From": message['From'],
-                "To": message['To'],
-                "Subject": message['Subject'],
-                "when": message['date'],
-                "body": message['body'],
+                "id": message["Message-Id"],
+                "X-GM-THRID": message["X-GM-THRID"],
+                "X-Gmail-Labels": message["X-Gmail-Labels"],
+                "From": message["From"],
+                "To": message["To"],
+                "Subject": message["Subject"],
+                "when": message["date"],
+                "body": message["body"],
             }
-
-                for message in get_mbox(mbox_file)
+            for message in get_mbox(mbox_file)
         ),
         pk="id",
         alter=True,
     )
-    print('Finished loading emails into {}.'.format(mbox_file))
+    print("Finished loading emails into {}.".format(mbox_file))
     print('Enabling full text search on "body" and "Subject" fields')
     db["mbox_emails"].enable_fts(["body", "Subject"])
-    print('Finished!')
+    print("Finished!")
 
 
 def get_email_body(message):
-    '''
+    """
     return the email body contents
-    '''
+    """
     body = None
     if message.is_multipart():
         for part in message.walk():
             if part.is_multipart():
                 for subpart in part.walk():
-                    if subpart.get_content_type() == 'text/plain':
+                    if subpart.get_content_type() == "text/plain":
                         body = subpart.get_payload(decode=True)
-            elif part.get_content_type() == 'text/plain':
+            elif part.get_content_type() == "text/plain":
                 body = part.get_payload(decode=True)
-    elif message.get_content_type() == 'text/plain':
+    elif message.get_content_type() == "text/plain":
         body = message.get_payload(decode=True)
     return body
 
@@ -170,8 +167,8 @@ def get_message_date(get_date, get_from):
     datetime_tuple = email.utils.parsedate_tz(mail_date)
     if datetime_tuple:
         unix_time = email.utils.mktime_tz(datetime_tuple)
-        mail_date_iso8601 = datetime.datetime.utcfromtimestamp(unix_time).isoformat(' ')
+        mail_date_iso8601 = datetime.datetime.utcfromtimestamp(unix_time).isoformat(" ")
     else:
-        mail_date_iso8601 = ''
+        mail_date_iso8601 = ""
 
     return mail_date_iso8601

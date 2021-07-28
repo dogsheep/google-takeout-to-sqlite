8ee555c2889a38ff42b95664ee074b4a01a82f06
Max Hawkins <maxhawkins@gmail.com>
2021-07-27 23:41:03 -0700

    Use thread id as pkey if missing message id.
    Some messages (like gchat logs) don't have message ids and therefore don't save properly. This commit uses the gmail X-GM-THRID if the Message-Id is missing.

--- google_takeout_to_sqlite/utils.py
+++ google_takeout_to_sqlite/utils.py
@@ -140,7 +140,7 @@ def save_emails(db, mbox_file):
     db["mbox_emails"].upsert_all(
         (
             {
-                "id": message["Message-Id"],
+                "id": message["Message-Id"] if "Message-Id" in message else message["X-GM-THRID"],
                 "X-GM-THRID": message["X-GM-THRID"],
                 "X-Gmail-Labels": message["X-Gmail-Labels"],
                 "From": message["From"],
